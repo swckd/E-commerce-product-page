052ecf5b8db3579fb12cd6ddec48e490a7c4e2b8
swckd <alejandroariasag@gmail.com>
2024-02-18 19:53:18 +0100

    End of day

---
+++ assets/scripts/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona todos los thumbnails
+    var thumbnails = document.querySelectorAll('.thumbnail');
+  
+    // Añade un event listener a cada thumbnail
+    thumbnails.forEach(function(thumbnail) {
+      thumbnail.addEventListener('click', function() {
+        // Obtiene el índice de la diapositiva a la que queremos ir
+        var slideIndex = this.getAttribute('data-bs-slide-to');
+  
+        // Obtiene una referencia al carrusel
+        var carousel = document.querySelector('#myCarousel');
+  
+        // Muestra el modal (si no está ya visible)
+        var modalEl = document.querySelector('#myModal');
+        var modal = new bootstrap.Modal(modalEl);
+        modal.show();
+  
+        // Cuando el modal se ha mostrado completamente...
+        modalEl.addEventListener('shown.bs.modal', function() {
+          // Navega a la diapositiva correcta
+          var carouselInstance = new bootstrap.Carousel(carousel);
+          carouselInstance.to(parseInt(slideIndex));
+        });
+      });
+    });
+  });
